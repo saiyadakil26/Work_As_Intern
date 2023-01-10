548724c1706ecc73f11ab316fec81806130664e4
Saiyad Akil <saiyadakil26@gmail.com>
2023-01-10 16:08:57 +0530

    Practice_03 completed..

--- Practice_03/program_04.js
+++ Practice_03/program_04.js
@@ -1,7 +1,8 @@
 var array = [1, 2];
-
 const result = (arr) => {
    return arr.reduce((a, v) =>{
+    // console.log(a);
+    // console.log(v);
         return a.concat(a.map(r => [v].concat(r)))
     } , [[]]);
 }
--- Practice_03/program_08.js
+++ Practice_03/program_08.js
@@ -1,23 +1,31 @@
-//in panding
-const sort_by=(arr,props,orders)=>
-    [...arr].sort((a, b) =>
-    props.reduce((acc, prop, i) => {
-      if (acc === 0) {
-        const [p1, p2] = orders && orders[i] === 'desc' ? [b[prop], a[prop]] : [a[prop], b[prop]];
-        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
-      }
-      return acc;
-    }, 0)
-)
+const sort_by=(arr,props,orders)=>{
+  // return [...arr].sort((a, b) =>
+  //   props.reduce((acc, prop, i) => {
+  //     if (acc === 0) {
+  //       const [p1, p2] = orders && orders[i] === 'desc' ? [b[prop], a[prop]] : [a[prop], b[prop]];
+  //       acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
+  //     }
+  //     return acc;
+  //   }, 0)
+  // )
 
+  //  return arr.sort((a,b)=>{
+  //     if (a.name > b.name) return orders[0] === 'asc' ? -1 : 1
+  //     if (a.name < b.name) return orders[0] === 'asc' ? 1 : -1
+  //     if (a.name == b.name) {
+  //       if (a.age > b.age) return orders[1] === 'asc' ? -1 : 1
+  //       if (a.age < b.age) return orders[1] === 'asc' ? 1 : -1
+  //     }
+  //  })
+  
+}
 let obj=[
-    {name:"pratik",age:19,collage:"ait"},
-    {name:"akil",age:19,collage:"dit"},
-    {name:"akil",age:20,collage:"cait"},
-    
+  {name:"pratik",age:19},
+  {name:"akil",age:19},
+  {name:"akil",age:20}, 
 ]
 
-let order_by=['age',"collage"]
+let order_by=['name',"age"]
 
 let order=['asc','desc']
 
