0b4105365d95491101367bb9b57bea422ccf284d
Saiyad Akil <saiyadakil26@gmail.com>
2023-01-12 13:17:52 +0530

    Practice_04 Updated.

--- Practice_04/path/path1.js
+++ Practice_04/path/path1.js
@@ -3,7 +3,7 @@ const path=require('path')
 console.log(path.basename(__dirname))
 console.log(path.dirname(__dirname));
 console.log(path.extname(__filename));
-console.log(path.join(__dirname,"\\hello"));
+console.log(path.join(__dirname,"\\hello.js"));
 let  obj = { dir: 'D:\\Pratice_\\Practice_04\\path', base: 'path.js' }
 console.log(path.format(obj));
 console.log(path.normalize(__dirname));
